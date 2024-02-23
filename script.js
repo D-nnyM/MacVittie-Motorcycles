7f4b5c4cbd35fe7bf47df03cfedd65a6f5096036
Daniel Muller <daniel.muller034@gmail.com>
2024-02-23 09:23:58 -0500

    revert to blank slate, make CSS more efficient to work with default browser settings

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = document.querySelector('body');
+const viewportWidth = window.innerWidth;
+const viewportHeight = window.innerHeight;
+
+// body.style.width = `${viewportWidth}px`;
+// body.style.height = `${viewportHeight}px`;
