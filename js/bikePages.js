449485a174b47125b41bd16475bee9bd262bb5a3
Daniel Muller <daniel.muller034@gmail.com>
2024-03-25 15:56:49 -0400

    add event listeners to each image to swap main image out with gallery image, assign height values to all containers to fix bugs

--- js/bikePages.js
+++ js/bikePages.js
@@ -1,2 +1,20 @@
-const mainImage = document.querySelector(".main-image");
+const galleryImages = document.querySelectorAll(".image-gallery img");
+
+
+for (let i = 0; i < galleryImages.length; i++) {
+
+    let mainImage = document.querySelector('.main-image');
+    let mainImageSrc = mainImage.getAttribute('src');
+
+    galleryImages[i].addEventListener('click', e => {
+        const galleryImageSrc = galleryImages[i].getAttribute('src');
+        
+        mainImage.src = galleryImageSrc;        
+    })
+
+
+
+   
+    
+}
 
